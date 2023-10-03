c2ad2ef56125692047b0a4afd847f7982f017ae9
Famidore <lokoroko456@gmail.com>
2023-10-03 14:15:37 +0200

    ant model && basic motion and rotation

--- code/ant.js
+++ code/ant.js
@@ -6,13 +6,15 @@ class Ant{
         this.size = tempSize;
         this.angle = 0;
 
-        this.modelPath = tempModelPath;
+        this.modelPath = antWorkerModel;
 
         this.xoff = 0;
         this.yoff = 0;
     }
 
     show(){
+
+        this.angle+=1;
         this.x = constrain(this.x, 0, width);
         this.y = constrain(this.y, 0, height);
 
@@ -25,13 +27,20 @@ class Ant{
         // noFill();
         // this.modelPath.resize(this.size, this.size * 2);
 
-        fill(255);
+        //fill(255);
+        noFill()
+        imageMode(CENTER)
+        this.modelPath.resize(this.size, this.size * 2)
+        image(this.modelPath, 0, 0)
+        //stroke(255);
         rect(0, 0, this.size, this.size * 2);
+
+        pop();
     }
 
     move(){
-        this.xoff += 0.02;
-        this.yoff += 0.01;
+        this.xoff += 0.006;
+        this.yoff += 0.003;
         var nx = noise(this.xoff) * width;
         var ny = noise(this.yoff) * height;
 
--- code/main.js
+++ code/main.js
@@ -1,11 +1,18 @@
 let ants = [];
 
+function preload(){
+
+  antWorkerModel = loadImage('assets/antModel1.png');
+}
+
 
 function setup() {
   createCanvas(800, 800);
 
-  for (let i = 0; i < 10; i++){
-    ants.push(new Ant(0, 0, 10));
+  for (let i = 0; i < 50; i++){
+    var x = random(50, width-50);
+    var y = random(50, height-50);
+    ants.push(new Ant(x, y, 10));
   }
 }
 
@@ -14,6 +21,6 @@ function draw() {
 
   for (ant of ants){
     ant.show();
-    ant.move();
+    // ant.move();
   }
 }
